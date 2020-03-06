ef01614cb12dd3cffbe220b5cefbb569d030b097
NicoleA505 <devnicole505@gmail.com>
2020-03-06 20:47:24 +0100

    Added beginning functionality for the addReview onSubmit
    Does add to the reviews array, but displays strange and name input returns undefined. Image is not the image assigned.

--- src/components/Review.js
+++ src/components/Review.js
@@ -9,7 +9,11 @@ const Review = (props) => {
          <div className="review">
             <div className="reviewAuthorInfo">
                 <div className="authorInfo">
-                    <img className="reviewAuthorImage" src={props.profile_photo_url} alt={props.author}/>
+                    <img
+                        className="reviewAuthorImage" 
+                        src={props.profile_photo_url} 
+                        alt={props.author} 
+                        />
                     <a className="reviewAuthorName" href={props.author_url}>{props.author}</a>
                 </div>
                 <p className="star-symbol">{char.repeat(props.rating)}</p>
--- src/components/ReviewsList.js
+++ src/components/ReviewsList.js
@@ -1,18 +1,53 @@
-import React, { Component } from 'react'
+import React, { Component, useReducer } from 'react'
 import Review from './Review'
 import axios from 'axios'
 
 
 class ReviewsList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            reviews: []
-        };
-    }
-      
+    state = {
+        reviews: [],
+        value: {
+            name: "",
+            text: ""
+        },
+    };
 
-     componentDidMount = () => {
+      
+    handleChangeName = (event) => {
+        this.setState({
+            value: {
+                name: event.target.value,
+            }
+        });
+        // console.log(this.state.value.name)
+    }
+
+    handleChangeText = (event) => {
+        this.setState({
+            value: {
+                text: event.target.value
+            }
+        });
+        // console.log(this.state.value.text)
+    }
+
+    handleSubmit = (event) => {
+        let newReview = {
+            author: this.state.value.name,
+            text: this.state.value.text,
+            author_url: './images/useReducer.png'
+
+        }
+        // let arr = ...this.state.reviews
+        this.setState({
+            reviews: this.state.reviews.concat(newReview)
+        })
+        console.log('A review was submitted: ' + this.state.value.name + this.state.value.text);
+        event.preventDefault();
+      }
+
+
+    componentDidMount = () => {
 
         const placeId = this.props.placeId
 
@@ -35,17 +70,7 @@ class ReviewsList extends Component {
                 })
             })
         }
-        
-        //the problem is here since it isn't even showing the console.log. BUT the ReviewsList component IS being rendered, which means that the reviews state is not being updated correctly!
         getReviews()
-            // .then( (reviews) => {
-            //     console.log("Hello getReviews is firing!")
-            //     this.setState({
-            //         reviews: reviews
-            //     })
-            // })
-
-        
       } //End of componentDidMount
 
     render() { 
@@ -64,12 +89,15 @@ class ReviewsList extends Component {
 
                     />
                 )}
-                <form>
-                    <div class="form-group">
-                        <textarea class="form-control" id="addReviewTextArea" rows="3" placeholder="Type your review here."></textarea>
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <h5>Add a Review:</h5>
+                        <input className="form-control addReviewName" id="addReviewAuthorName" type="text" placeholder="Your name:" value={this.state.value.name} onChange={this.handleChangeName} />
+                        <textarea className="form-control addReviewText" id="addReviewTextArea" rows="3" placeholder="Type your review here." value={this.state.value.text} onChange={this.handleChangeText}></textarea>
                     </div>
+                    <button className="btn btn-info btn-sm submitButton" type="submit" value="Submit">Add Review</button>  
                 </form>
-                <button className="btn btn-info btn-sm submitButton" type="submit">Add a Review</button>   
+ 
             </div>
          );
     }
